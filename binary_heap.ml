bac73d8b3b67f5f7d9298f35995e3524808a800a
mynz <rollingteapot@gmail.com>
2013-11-14 02:59:33 +0900

    heap

--- binary_heap.ml
+++ binary_heap.ml
@@ -1,5 +1,5 @@
-let h = [ 1; 2; 3];;
-let a = Array.of_list h;;
+let l = [ 1; 2; 3];;
+let a = Array.of_list l;;
 
 let print_list l =
     let s =  String.concat ", " (List.map string_of_int l)
@@ -8,6 +8,32 @@ let print_list l =
 let to_array = Array.of_list;;
 let to_list = Array.to_list;;
 
-print_list h;;
+(* print_list l;; *)
+(* ---- *)
+
+type heap = Null | Lf of int | Br of int * heap * heap;;
+
+(* let h = Null;; *)
+let h = Br (7, Null, Null)
+
+
+let rec print_heap = function
+    Null -> "[null]"
+    | Lf v -> ("[" ^ (string_of_int v) ^ "]")
+    | Br (v, l, r) -> ("[br: L: " ^ (string_of_int v) ^ (print_heap l) ^ ", R: " ^ (print_heap r)  ^ "]");;
+
+
+print_string (print_heap h);;
+
+
+
+let up h v = 
+    Br (v, Null, Null) ;;
+
+
+
+
+
+
 
 
