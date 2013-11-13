f3c7cb5002206f56c9ff5b8d69cd523578b18a99
mynz <rollingteapot@gmail.com>
2013-11-14 00:47:45 +0900

    snap

--- binary_heap.ml
+++ binary_heap.ml
@@ -5,21 +5,9 @@ let print_list l =
     let s =  String.concat ", " (List.map string_of_int l)
     in print_string ("[" ^ s ^ "]");;
 
+let to_array = Array.of_list;;
+let to_list = Array.to_list;;
+
 print_list h;;
 
-
-(* let print_array a =  *)
-    (* Array.map string_of_int a;; *)
-
-
-(* Array.iter (fun a -> a) (Array.map string_of_int a);; *)
-
-
-(* let sl = String.concat ", " (Array.map string_of_int a) *)
-    (* in print_string sl;; *)
-
-(* Array.iter print_int a;; *)
-
-(* let s = List.fold_left (fun a b -> a ^ string_of_int b ^ ", ") "" h *)
-    (* in print_string ("(" ^  s ^ ")");; *)
 
